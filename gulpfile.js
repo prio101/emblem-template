3e4c530df7a55bcc6609d7fdff5f5b359c723f25
prio101 <miprio101@gmail.com>
2016-02-23 10:07:14 +0600

    Added GulpFile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,36 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var imageMin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var browserSync = require('browser-sync').create();
 
+//gulp sass
+
+gulp.task('sass' , function(){
+    return gulp.src('./sass/**/*.scss')
+            .pipe(sass().on('error' , sass.logError))
+            .pipe(gulp.dest('/css'));
+});
+
+gulp.task('imagemin', function(){
+    return gulp.src('./images/*')
+        .pipe(imageMin({
+            svgoPlugins: [
+                {removeViewBox: false},
+                {cleanupIds: false}
+            ],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'))
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+gulp.task('default', ['sass','imagemin' , 'browserSync']);
